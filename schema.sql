25c835989b21b2a7c55c6090d9e06ecef23c8632
gsass <george.a.sass@gmail.com>
2013-08-28 15:55:50 -0400

    Added a table to represent the navigation hierarchy to schema.sql

--- schema.sql
+++ schema.sql
@@ -1,7 +1,14 @@
 drop table if exists pages;
 create table pages (
+  id Integer not null auto_increment
   title text primary key not null,
   blurb text not null,
   imagename text,
   content text
+);
+
+drop table is exists nav;
+create table nav (
+  id Integer not null auto_increment
+  children text not null
 );
