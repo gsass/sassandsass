0ffdd4e3aa5aff8c6baf7c30a4d83deaf359b5df
gsass <george.a.sass@gmail.com>
2013-08-30 18:04:43 -0400

    Created the jinja template to render the navigation bar.

--- sassandsass/navbar.py
+++ sassandsass/navbar.py
@@ -4,12 +4,12 @@ from flask import g
 def generate_navbar():
     navbar={}
     pages = g.db.execute('SELECT id, children from nav order by rank desc')
-    navbar = [dict(page=get_page_descriptor(row[0]), children=get_page_descriptor(row[1])) 
+    navbar = [dict(page=get_page_descriptor(row[0])[0], children=get_page_descriptor(row[1])) 
              for row in pages.fetchall()]
     return navbar
 
 def get_page_descriptor(ids):
     pages = ids.split(',')
-    descriptors = g.db.execute("SELECT title, link_alias FROM pages WHERE id IN (?);", ids)
+    descriptors = g.db.execute("SELECT link_alias, title FROM pages WHERE id IN (?);", ids)
 
     links = [(desc[0], desc[1]) for desc in descriptors.fetchall()]
