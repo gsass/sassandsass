68dd95a6df08b0687f6cbfadc1b0bdffb55427b7
gsass <george.a.sass@gmail.com>
2013-10-17 17:58:57 -0400

    Moved the list of scripts and CSS files into config.

--- sassandsass/config.py
+++ sassandsass/config.py
@@ -3,6 +3,8 @@ class Config:
     SECRET_KEY = 'cream'
     LANDING_PAGE = 'aboutus'
     DEBUG = False
+    STYLES = ["structure.css", "content.css", "nav.css" ]
+    SCRIPTS = ["jquery-1.10.2.min.js","jquery.nav.js"]
     
 class DebugConfig(Config):
     DEBUG = True
