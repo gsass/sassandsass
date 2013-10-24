24fa434c72215edefc270eba6e50e61e53071cb1
gsass <george.a.sass@gmail.com>
2013-10-24 14:59:17 -0400

    Added the edit form loader to the list of scripts to load in the header.

--- sassandsass/config.py
+++ sassandsass/config.py
@@ -4,7 +4,7 @@ class Config:
     LANDING_PAGE = 'aboutus'
     DEBUG = False
     STYLES = ["structure.css", "content.css", "nav.css" ]
-    SCRIPTS = ["jquery-1.10.2.min.js","jquery.nav.js"]
+    SCRIPTS = ["jquery-1.10.2.min.js","jquery.nav.js",'edit_form_loader.js']
     MEDIA_ICONS = {"Twitter": "https://twitter.com/sassandsass",
             "LinkedIn": "https://www.linkedin.com/company/3269338?trk=tyah"}
     
