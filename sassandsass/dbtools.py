97e4ae99bed92053b6f377842e3d0c4733b2902c
gsass <george.a.sass@gmail.com>
2013-10-31 14:41:14 -0400

    Added a generic function to handle non-SELECT calls to the database, and functions to handle editing images on pages.

--- sassandsass/dbtools.py
+++ sassandsass/dbtools.py
@@ -1,7 +1,9 @@
 import sqlite3
-from flask import g
+from flask import g, flash
+from werkzeug import secure_filename
 from contextlib import closing
 from sassandsass import app
+from sassandsass.images import upload_image
 
 
 def init_db():
@@ -23,6 +25,19 @@ def teardown_request(exception):
     if db is not None:
         db.close()
 
+def db_edit_call(call, fields, msg, flash_msg=False):
+    try:
+        g.db.execute(call, fields)
+    except sqlite3.Error as error:
+        msg = "%s...%s" % (msg, error)
+    g.db.commit()
+    msg = "%s...Success!" % msg
+    if not flash_msg:
+        return msg
+    flash(msg)
+
+#PAGE MANAGEMENT FUNCTIONS
+
 def page_exists(pagename):
     page_id = g.db.execute("SELECT id FROM pages WHERE link_alias = ?",
             (pagename,))
@@ -35,16 +50,43 @@ def fetch_page_content(pagename):
 
 @app.context_processor
 def get_available_pages():
-    cur = g.db.execute("SELECT id, title from pages "+
-                        "WHERE id NOT IN (SELECT id FROM nav)")
-    return dict(available_pages = cur.fetchall())
+    def inner():
+        cur = g.db.execute("SELECT id, title from pages "+
+                            "WHERE id NOT IN (SELECT id FROM nav)")
+        return cur.fetchall()
+    return dict(available_pages = inner)
+
+def update_page_content(form):
+    msg = "Editing the %s on %s" % (form["section"], form["page"])
+    section = form["section"]
+    if section in ("title", "blurb", "content"):
+        call = "UPDATE pages SET {}=:edited WHERE link_alias=:page".format(
+                section)
+        msg = db_edit_call(call, form, msg)
+    else:
+        msg = "{}...{} is not editable.".format(msg, section)
+    return msg
+
+def update_page_image(form, files):
+    msg = "Updating the image on {}".format(form["page"])
+    img = files.get("img")
+    if img:
+        imgname = upload_image(img)
+        if imgname:
+            form["imgname"]=imgname
+        else:
+            return "{}...image upload failed.".format(msg)
+    return db_edit_call("UPDATE pages SET imagename=:imgname "+
+            "WHERE link_alias=:page", form, msg)
+
+#USER MANAGEMENT FUNCTIONS
 
 def get_user_info(**kwargs):
     if 'userid' in kwargs:
         with closing(connect_db()) as db:
             try:
-                cur = db.execute("SELECT userid, active, tokenhash FROM users "+
-                    "WHERE userid = :userid", kwargs)
+                cur = db.execute("SELECT userid, active, tokenhash "+
+                        "FROM users WHERE userid = :userid", kwargs)
                 return cur.fetchone()
             except sqlite3.OperationalError:
                 return None
